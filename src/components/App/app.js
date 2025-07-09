44cbadac4b9c8d35c2ee822219dffc46541540eb
Denys Borysenko <denisborisenko479@gmail.com>
2025-07-09 18:11:24 +0300

    Додано компоненти згідно з ЛР-6

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,9 +1,44 @@
+import Title from "../Title/title";
+import Container from "../Container/container";
+import './app.css';
+import Form from "../Form/form";
+import { useState } from "react";
+import TodoList from "../TodoList/todoList";
+
 function App() {
+  const [todos, setTodos] = useState([]); // Додаємо локальний стейт
+
   return (
-    <div>
-      Hello, World!
+    <div className={'app'}>
+      <Container>
+        <div className={'title-wrap'}>
+          <Title>Todo</Title>
+        </div>
+
+        <div className={'form-wrap'}>
+          <Form
+            saveTodo={
+              (todoText) => {
+                const trimmedText = todoText.trim();
+                if (trimmedText.length > 0) {
+                  setTodos([...todos, trimmedText]);
+                }
+              }
+            }
+          />
+        </div>
+
+        <TodoList
+          todos={todos}
+          removeTodo={
+            (todoIndex) => {
+              const newTodos = todos.filter((_, index) => index !== todoIndex);
+              setTodos(newTodos);
+            }
+          }
+        />
+      </Container>
     </div>
   );
 }
-
 export default App;
